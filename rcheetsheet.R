29688db769e16d0d1d9137b7b2e3c7c4ad686c96
civelldr <civelldr@users.noreply.github.com>
2014-09-11 15:38:14 -0700

    updated for swirl assignment 1

--- rcheetsheet.R
+++ rcheetsheet.R
@@ -9,3 +9,27 @@ nrows(data)
 tail(data,n=2)
 summary(data)
 subsetData <- subset(data, Ozone > 31 & Temp > 90 )
+
+# swirl
+y <- x[!is.na(x) & x > 0]               # only the non-NA values in x that are > 0
+my_data <- sample(c(y, z), 100)         # sample 100 values at random from vectors y and z
+vect <- c(foo = 11, bar = 2, norf = NA) # named elements
+tapply(flags$population, flags$landmass, summary) # summarize the population data by landmass
+
+## data frame summary
+dim(plants)
+obj <- object.size(plants)
+print(obj,units="Mb")
+str(plants) 
+summary(plants)
+table(plants$Active_Growth_Period)
+
+flips <- sample(c(0,1), size=100, replace=TRUE, prob=c(0.3, 0.7))
+rbinom(1,size=100, prob=0.7) # number of heads in 100 tosses
+rbinom(100,size=1, prob=0.7) # vector of coin flips where p of rolling 1 is 0.7
+rnorm(n=10,mean=100,sd=25) # 10 random numbers from a dist with mean of 100 and sd of 25
+my_pois <- replicate(100, rpois(5,10)) # 100 replicates of 5 numbers from a poisson dist with mean of 10
+
+# dates
+t3 <- "October 17, 1986 08:24"
+t4 <- strptime(t3, "%B %d, %Y %H:%M")
